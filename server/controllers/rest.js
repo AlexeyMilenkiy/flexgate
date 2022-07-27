6f0a2545fe7e545d053013562a61193da0ecb1af
AlexeyMilenkiy <alex.milenkiy@gmail.com>
2022-07-27 11:03:38 +0300

    app/server: refactoring

--- server/controllers/rest.js
+++ server/controllers/rest.js
@@ -1,33 +1,29 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({ path: '../.env' });
 
-const getMessage = ({
-  name,
-  tel,
-  model,
-  comment,
-  checked,
-}) => `<strong>Заказ с сайта Flexgate.ru</strong>\n
-<strong>name</strong> ${name}\n
-<strong>tel</strong> ${tel}\n
-<strong>model</strong> ${model}\n
-<strong>comment</strong> ${comment}\n
-${checked ? '\n<strong>ПЕРЕЗВОНИТЬ</strong>\n' : ''}`;
+const chat_id = process.env.TELEGRAM_CHAT;
+const token = process.env.TELEGRAM_TOKEN;
+
+const getMessage = ({ name, tel, model, comment, checked }) => `
+<b>Заказ с сайта Flexgate.ru</b>\n
+<b>name</b> ${name}\n
+<b>tel</b> ${tel}\n
+<b>model</b> ${model}\n
+<b>comment</b> ${comment}\n
+${checked ? '\n*ПЕРЕЗВОНИТЬ*\n' : ''}
+`;
 
 export const sendMessageToBot = async (req, res) => {
   try {
-    const message = getMessage(req.body);
-    await axios.post(
-      `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage?parse_mode=html`,
-      null,
-      {
-        params: {
-          chat_id: process.env.TELEGRAM_CHAT,
-          text: message,
-        },
-      }
-    );
+    const text = getMessage(req.body);
+    await axios.post(`https://api.telegram.org/bot${token}/sendMessage`, null, {
+      params: {
+        parse_mode: 'html',
+        chat_id,
+        text,
+      },
+    });
     res.status(200).end();
   } catch (error) {
     res.status(400).send(error.message);
